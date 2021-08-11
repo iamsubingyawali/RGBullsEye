a89e1294d72b34f8658a7082115340d7ac1a893d
iamsubingyawali <subingyawali7@gmail.com>
2021-08-11 11:29:43 +0545

    Initial Commit

---
+++ RGBullsEye/RGBullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBullsEyeApp.swift
+//  RGBullsEye
+//
+//  Created by Subin Gyawali on 8/11/21.
+//
+
+import SwiftUI
+
+@main
+struct RGBullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
