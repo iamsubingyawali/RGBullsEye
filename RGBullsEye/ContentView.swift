10fc59cc61e6e5a6ca60dc4595528155a31617ae
iamsubingyawali <subingyawali7@gmail.com>
2021-08-11 12:01:42 +0545

    Few componenets added

--- RGBullsEye/ContentView.swift
+++ RGBullsEye/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()	
+        VStack {
+            Color(red: 0.5, green: 0.5, blue: 0.5)
+            Text("R: ??? G: ??? B: ???")
+                .padding()
+            Color(red: 0.5, green: 0.5, blue: 0.5)
+            Text("R: ??? G: ??? B: ???")
+                .padding()
+        }
     }
 }
 
